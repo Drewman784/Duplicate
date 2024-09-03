ae766a062cc4b3bcc25d3fe7d85a121274edb86a
Drew <89123942+Drewman784@users.noreply.github.com>
2024-09-02 22:25:53 -0400

    Add files via upload

---
+++ Assets/Script/DupliScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DupliScript: MonoBehaviour
+{
+
+    public GameObject Player;
+
+    //Duplicate Script
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        GameObject duplicate = Instantiate(Player, new Vector3(transform.position.x + 1, transform.position.y, transform.position.z), Quaternion.Euler(new Vector3(0, 0, -90)));
+        duplicate.GetComponent<Rigidbody2D>();
+    }
+}
---
+++ Assets/Script/WinScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScript: MonoBehaviour
+{
+    public string Scenename;
+
+    //call if level must be changed.
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        SceneManager.LoadScene(Scenename);
+    }
+}
